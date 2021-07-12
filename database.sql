5e19bf016520072fa256d2eea4785e328820397e
Lucas <Lucas-cs1998@hotmail.com>
2021-07-12 12:56:06 -0300

    Database schema finished

--- database.sql
+++ database.sql
@@ -16,7 +16,7 @@ create table Raca (
 
 create table DeslocamentoRaca (
 	nomeRaca varchar(30),
-	nomeDesclocamento varchar(25),
+	nomeDeslocamento varchar(25),
 	valor integer not null,
 	primary key(nomeRaca, nomeDeslocamento)
 );
@@ -33,7 +33,7 @@ create table TracoEspecialRaca (
 	nomeRaca varchar(30),
 	descricao text not null,
 	primary key (nome, nomeRaca),
-	foreign key (nomeRaca) references (nomeRaca)
+	foreign key (nomeRaca) references Raca (nomeRaca)
 );
 
 create table Monstro (
@@ -48,7 +48,7 @@ create table Monstro (
 	formaCorporal varchar(20) not null,
 	tamanho varchar(20) not null,
 	descricaoAcaoLendaria text not null,
-	primary key (nomeMonstro),
+	primary key (nomeMonstro)
 );
 
 create table DeslocamentoMonstro (
@@ -62,8 +62,8 @@ create table DeslocamentoMonstro (
 create table IdiomaMonstro (
 	nomeMonstro varchar(40),
 	nomeIdioma varchar(25) not null,
-	primary key nomeMonstro,
-	foreign key nomeMonstro references Monstro(nomeMonstro)
+	primary key (nomeMonstro),
+	foreign key (nomeMonstro) references Monstro(nomeMonstro)
 );
 
 create table SentidoMonstro (
@@ -111,7 +111,7 @@ create table Classe (
 	descricao text not null,
 	pontosVidaInicial varchar(50) not null,
 	progressaoVida varchar(50) not null,
-	primary key (nomeClasse),
+	primary key (nomeClasse)
 );
 
 create table Proficiencia (
@@ -149,7 +149,7 @@ create table Bruxo (
 	magiasConhecidas integer not null,
 	truquesConhecidos integer not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table Barbaro (
@@ -158,7 +158,7 @@ create table Barbaro (
 	danoFuria integer not null,
 	furias integer not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table Bardo (
@@ -167,7 +167,7 @@ create table Bardo (
 	truquesConhecidos integer not null,
 	magiasConhecidas integer not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table ClerigoDruidaMago (
@@ -175,7 +175,7 @@ create table ClerigoDruidaMago (
 	nivel integer,
 	truquesConhecidos integer not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table Feiticeiro (
@@ -185,7 +185,7 @@ create table Feiticeiro (
 	truquesConhecidos integer not null,
 	magiasConhecidas integer not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)	
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table Monge (
@@ -195,7 +195,7 @@ create table Monge (
 	artesMarciais varchar(5) not null,
 	pontosChi integer not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table Ladino (
@@ -203,7 +203,7 @@ create table Ladino (
 	nivel integer,
 	ataqueFurtivo varchar(5) not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table Patrulheiro (
@@ -211,7 +211,7 @@ create table Patrulheiro (
 	nivel integer,
 	magiasConhecidas integer not null,
 	primary key (nomeClasse, nivel),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table EspacoMagia (
@@ -220,7 +220,7 @@ create table EspacoMagia (
 	nivelMagia integer,
 	quantidade integer not null,
 	primary key (nomeClasse, nivel, nivelMagia),
-	foreign key (nomeClasse, nivel) references Atributo (nivel, nomeClasse)
+	foreign key (nomeClasse, nivel) references Atributo (nomeClasse, nivel)
 );
 
 create table Magia (
@@ -232,13 +232,13 @@ create table Magia (
 	componentes varchar(15) not null,
 	alcance varchar(20) not null,
 	tempoConjuracao varchar(20) not null,
-	primary key (nomeMagia),
+	primary key (nomeMagia)
 );
 
 create table ClasseMagia (
 	nomeClasse varchar(20),
 	nomeMagia varchar(35),
-	primary key (nomeClasse, nomeMagia)
+	primary key (nomeClasse, nomeMagia),
 	foreign key (nomeClasse) references Classe (nomeClasse),
 	foreign key (nomeMagia) references Magia (nomeMagia)
 );
@@ -254,8 +254,8 @@ create table ClassePericia (
 	nomeClasse varchar(20),
 	nomePericia varchar(30),
 	primary key (nomeClasse, nomePericia),
-	foreign key nomeClasse references Classe(nomeClasse),
-	foreign key nomePericia references Pericia(nomePericia)
+	foreign key (nomeClasse) references Classe(nomeClasse),
+	foreign key (nomePericia) references Pericia(nomePericia)
 );
 
 create table Item (
@@ -263,7 +263,7 @@ create table Item (
 	descricao text not null,
 	categoria varchar(20) not null,
 	custo integer not null,
-	moeda varchar(2) not null,
+	moeda char(2) not null,
 	primary key (nomeItem)
 );
 
@@ -272,7 +272,7 @@ create table Arma (
 	tipo varchar(50) not null,
 	dano varchar(20) not null,
 	peso integer not null,
-	primery key (nomeItem),
+	primary key (nomeItem),
 	foreign key (nomeItem) references Item (nomeItem)
 );
 
@@ -287,7 +287,7 @@ create table PropriedadeArma (
 create table Montaria (
 	nomeItem varchar(35),
 	velocidade integer not null,
-	capacidadeCarregar not null,
+	capacidadeCarregar integer not null,
 	primary key (nomeItem),
 	foreign key (nomeItem) references Item(nomeItem)
 );
@@ -343,7 +343,7 @@ create table Antecedente (
 create table AntecedentePericia (
 	nomeAntecedente varchar(30),
 	nomePericia varchar(30),
-	primary key (nomeAntecedente, nomePericia).
+	primary key (nomeAntecedente, nomePericia),
 	foreign key (nomeAntecedente) references Antecedente (nomeAntecedente),
 	foreign key (nomePericia) references Pericia (nomePericia)
 );
@@ -376,4 +376,265 @@ create table AntecedenteItem (
 	primary key (nomeAntecedente, nomeItem),
 	foreign key (nomeAntecedente) references Antecedente (nomeAntecedente),
 	foreign key (nomeItem) references Item (nomeItem)
+);
+
+create table Talento (
+	nomeTalento varchar(40),
+	preRequisito varchar(100) not null,
+	descricao text not null,
+	primary key (nomeTalento)
+);
+
+create table Plano (
+	nomePlano varchar(50),
+	descricao text not null,
+	corCortina varchar(20),
+	corPoco varchar(20),
+	primary key (nomePlano)
+);
+
+create table Regra (
+	nomeRegra varchar(50),
+	nomePlano varchar(50),
+	descricao text not null,
+	primary key (nomeRegra, nomePlano),
+	foreign key (nomePlano) references Plano (nomePlano)	
+);
+
+create table Campanha (
+	nomeCampanha varchar(100),
+	descricao text,
+	mapaAtual integer,
+	primary key (nomeCampanha)
+);
+
+create table Mapa (
+	codigoMapa serial,
+	nomeCampanha varchar(100) not null,
+	mapa varchar(150) not null,
+	primary key (codigoMapa),
+	foreign key (nomeCampanha) references Campanha (nomeCampanha)
+);
+
+create table Cidade (
+	codigoCidade serial,
+	nomeCidade varchar(50) not null,
+	nomeCampanha varchar(100) not null,
+	comercio varchar(50) not null,
+	clima varchar(30) not null,
+	vegetacao varchar(30) not null,
+	populacao integer not null,
+	formaGoverno varchar(20) not null,
+	primary key (codigoCidade),
+	foreign key (nomeCampanha) references Campanha (nomeCampanha)	
+);
+
+create table Lider (
+	codigoLider serial,
+	nomeLider varchar(50) not null,
+	descricao text,
+	primary key (codigoLider)
+);
+
+create table Faccao (
+	codigoFaccao serial,
+	nomeFaccao varchar(30) not null,
+	nomeCampanha varchar(100) not null,
+	formaGoverno varchar(20) not null,
+	primary key (codigoFaccao),
+	foreign key (nomeCampanha) references Campanha (nomeCampanha)	
+);
+
+create table LiderCidade (
+	codigoCidade integer,
+	codigoLider integer,
+	primary key (codigoCidade, codigoLider),
+	foreign key (codigoCidade) references Cidade (codigoCidade),
+	foreign key (codigoLider) references Lider (codigoLider)
+);
+
+create table LiderFaccao (
+	codigoFaccao integer,
+	codigoLider integer,
+	primary key (codigoFaccao, codigoLider),
+	foreign key (codigoFaccao) references Faccao (codigoFaccao),
+	foreign key (codigoLider) references Lider (codigoLider)
+);
+
+create table Deus (
+	codigoDeus serial,
+	nomeDeus varchar(50) not null,
+	dominio varchar(100) not null,
+	tendencia varchar(20) not null,
+	simbolo varchar(100) not null,
+	primary key (codigoDeus)
+);
+
+create table Anotacao (
+	codigoAnotacao serial,
+	nomeCampanha varchar(100) not null,
+	anotacao text,
+	primary key (codigoAnotacao),
+	foreign key (nomeCampanha) references Campanha (nomeCampanha)
+);
+
+create table Mundo (
+	nomeCampanha varchar(100),
+	nomePlano varchar(50),
+	primary key (nomeCampanha, nomePlano),
+	foreign key (nomeCampanha) references Campanha (nomeCampanha),
+	foreign key (nomePlano) references Plano (nomePlano)
+);
+
+create table PanteaoCampanha (
+	nomeCampanha varchar(100),
+	codigoDeus integer,
+	primary key (nomeCampanha, codigoDeus),
+	foreign key (nomeCampanha) references Campanha (nomeCampanha),
+	foreign key (codigoDeus) references Deus (codigoDeus)
+);
+
+create table Aparencia (
+	codigoAparencia serial,
+	altura numeric(7, 2),
+	peso numeric(7, 2),
+	foto varchar(150),
+	corOlhos varchar(20),
+	idade integer,
+	corPele varchar(20),
+	corCabelo varchar(20),
+	primary key (codigoAparencia)
+);
+
+create table Personagem (
+	codigoPersonagem serial,
+	nomeJogador varchar(30) not null,
+	nomeCampanha varchar(100) not null,
+	nomePersonagem varchar(30),
+	personagemRaca varchar(30) not null,
+	personagemClasse varchar(20) not null,
+	arquetipo varchar(30),
+	nomeAntecedente varchar(30),
+	coordenadaX integer,
+	coordenadaY integer,
+	tendencia varchar(20) not null,
+	percepcaoPassiva integer not null,
+	numeroInspiracao integer,
+	pontosVidaAtual integer,
+	pontosExperiencia integer,
+	capacidadeCarga integer,
+	cobre integer,
+	prata integer,
+	ouro integer,
+	platina integer,
+	electro integer,
+	codigoDeus integer,
+	codigoAparencia integer,
+	primary key (codigoPersonagem),
+	foreign key (nomeCampanha) references Campanha (nomeCampanha),
+	foreign key (personagemRaca) references Raca (nomeRaca),
+	foreign key (personagemClasse) references Classe (nomeClasse),
+	foreign key (nomeAntecedente) references Antecedente (nomeAntecedente),
+	foreign key (codigoDeus) references Deus (codigoDeus),
+	foreign key (codigoAparencia) references Aparencia (codigoAparencia)
+);
+
+create table PersonagemCaracteristicaAdicional (
+	codigoPersonagem integer,
+	caracteristicaAdicional text not null,
+	primary key (codigoPersonagem),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem)
+);
+
+create table PersonagemVinculo (
+	codigoPersonagem integer,
+	vinculo text not null,
+	primary key (codigoPersonagem),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem)
+);
+
+create table PersonagemDefeito (
+	codigoPersonagem integer,
+	defeito text not null,
+	primary key (codigoPersonagem),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem)
+);
+
+create table PersonagemIdeal (
+	codigoPersonagem integer,
+	ideal text not null,
+	primary key (codigoPersonagem),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem)
+);
+
+create table PersonagemTracoPersonalidade (
+	codigoPersonagem integer,
+	tracoPersonalidade text not null,
+	primary key (codigoPersonagem),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem)
+);
+
+create table PontosExperienciaNivel ( -- REVISAR ESSA PARTE, PROCURAR NA INTERNET COMO FAZER PONTOS DE XP E NIVEL EM BD RELACIONAL
+	pontosExperiencia integer,
+	nivel integer not null,
+	primary key (pontosExperiencia)
+);
+
+create table PersonagemTalento (
+	codigoPersonagem integer,
+	nomeTalento varchar(30),
+	primary key (codigoPersonagem, nomeTalento),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem),
+	foreign key (nomeTalento) references Talento (nomeTalento)
+);
+
+create table PersonagemItem (
+	codigoPersonagem integer,
+	nomeItem varchar(30),
+	quantidade integer,
+	equipado boolean,
+	primary key (codigoPersonagem, nomeItem),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem),
+	foreign key (nomeItem) references Item (nomeItem)
+);
+
+create table PersonagemPericia (
+	codigoPersonagem integer,
+	nomePericia varchar(30),
+	modificador integer not null,
+	primary key (codigoPersonagem, nomePericia),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem),
+	foreign key (nomePericia) references Pericia (nomePericia)
+);
+
+create table PersonagemMagia (
+	codigoPersonagem integer,
+	nomeMagia varchar(30),
+	primary key (codigoPersonagem, nomeMagia),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem),
+	foreign key (nomeMagia) references Magia (nomeMagia)
+);
+
+create table Habilidade (
+	valor integer,
+	valorModificador integer not null,
+	primary key (valor)
+);
+
+create table MonstroHabilidade (
+	nomeMonstro varchar(40),
+	nomeHabilidade varchar(15),
+	valor integer not null,
+	primary key (nomeMonstro, nomeHabilidade),
+	foreign key (nomeMonstro) references Monstro (nomeMonstro),
+	foreign key (valor) references Habilidade (valor)
+);
+
+create table PersonagemHabilidade (
+	codigoPersonagem integer,
+	nomeHabilidade varchar(15),
+	valor integer not null,
+	primary key (codigoPersonagem, nomeHabilidade),
+	foreign key (codigoPersonagem) references Personagem (codigoPersonagem),
+	foreign key (valor) references Habilidade (valor)
 );
