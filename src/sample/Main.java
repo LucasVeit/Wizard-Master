9f54014d32e12292cf2c8defb320883c6c6c7976
AmandaIsrael <amanda_israel@hotmail.com>
2021-07-27 18:03:13 -0300

    Tela início

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("databaseGenerica.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1315, 810));
         primaryStage.show();
