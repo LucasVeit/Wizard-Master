a554f2eecdbd7510808f4ea6cb9da7467390df80
Lucas <Lucas-cs1998@hotmail.com>
2021-08-30 15:51:43 -0300

    Magic is in the air!

--- src/Controller/databaseGenericaController.java
+++ src/Controller/databaseGenericaController.java
@@ -18,12 +18,15 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
 import sample.main;
+
+import javax.swing.text.TabableView;
 
 public class databaseGenericaController implements Initializable, controlledScreen {
 
@@ -35,8 +38,14 @@ public class databaseGenericaController implements Initializable, controlledScre
 
     //configure the table
     @FXML private TableView<Magia> tableView;
-    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn;
-    @FXML private TableColumn<Magia, SimpleStringProperty> tableDescricao;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn1;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn2;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn3;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn4;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn5;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn6;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn7;
+    @FXML private TableColumn<Magia, SimpleStringProperty> tableColumn8;
    // private ObservableList<ObservableList> data;
     @FXML private TextField barraPesquisa;
     @FXML
@@ -75,18 +84,46 @@ public class databaseGenericaController implements Initializable, controlledScre
 
     @FXML
     private void Pesquisar(ActionEvent event){
+        tableView.getColumns().clear();
         pesquisa = barraPesquisa.getText();
-        System.out.println(pesquisa);
+        //System.out.println(pesquisa);
         categoria = comboBox01.getValue();
-        System.out.println(categoria);
+        //System.out.println(categoria);
         atributo = comboBox02.getValue();
-        System.out.println(atributo);
-    }
+        //System.out.println(atributo);
 
-    public ObservableList<Magia> getMagia(){
-        MagiaDAO magiaDAO = new MagiaDAO();
-        ObservableList<Magia> magia = FXCollections.observableArrayList(magiaDAO.List());
-        return magia;
+        if(categoria == "Magia"){ // nao precisa fazer isso toda vez se cada tablecolumn for ser fixo uma tablecolumn de magia
+            tableColumn1 = new TableColumn<>("Nome Magia");
+            tableColumn2 = new TableColumn<>("Tipo");
+            tableColumn3 = new TableColumn<>("Nível");
+            tableColumn4 = new TableColumn<>("Descrição");
+            tableColumn5 = new TableColumn<>("Duração");
+            tableColumn6 = new TableColumn<>("Componentes");
+            tableColumn7 = new TableColumn<>("Alcance");
+            tableColumn8 = new TableColumn<>("Tempo Conjuração");
+            tableColumn1.setCellValueFactory(new PropertyValueFactory<>("nomeMagia"));
+            tableColumn2.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+            tableColumn3.setCellValueFactory(new PropertyValueFactory<>("nivel"));
+            tableColumn4.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+            tableColumn5.setCellValueFactory(new PropertyValueFactory<>("duracao"));
+            tableColumn6.setCellValueFactory(new PropertyValueFactory<>("componentes"));
+            tableColumn7.setCellValueFactory(new PropertyValueFactory<>("alcance"));
+            tableColumn8.setCellValueFactory(new PropertyValueFactory<>("tempoConjuracao"));
+            tableColumn1.setPrefWidth(100);
+            tableColumn2.setPrefWidth(100);
+            tableColumn2.setPrefWidth(100);
+            tableColumn3.setPrefWidth(100);
+            tableColumn4.setPrefWidth(100);
+            tableColumn5.setPrefWidth(100);
+            tableColumn6.setPrefWidth(100);
+            tableColumn7.setPrefWidth(100);
+            tableColumn8.setPrefWidth(100);
+            tableView.getColumns().addAll(tableColumn1, tableColumn2, tableColumn3, tableColumn4,
+                    tableColumn5, tableColumn6, tableColumn7, tableColumn8);
+
+            ObservableList<Magia> magias = FXCollections.observableArrayList(MagiaDAO.SearchByText(pesquisa, categoria, atributo));
+            tableView.setItems(magias);
+        }
     }
 
     // This method will return an ObservableList of People objects
@@ -103,14 +140,40 @@ public class databaseGenericaController implements Initializable, controlledScre
         return monstro;
     }
 
-    public void InsertChoiceBox(){
-        comboBox01.getItems().add("Apples");
-        comboBox01.getItems().add("Bananas");
-        comboBox01.getItems().addAll("Bacon", "Ham", "Meatballs");
+//    @FXML
+//    public void clickItem(MouseEvent event)
+//    {
+//        if (event.getClickCount() == 2) //Checking double click
+//        {
+//            System.out.println(TableView.getSelectionModel().getSelectedItem().getBeer());
+//            System.out.println(TableView.getSelectionModel().getSelectedItem().getBrewery());
+//            System.out.println(TableView.getSelectionModel().getSelectedItem().getCountry());
+//        }
+//    }
 
-        comboBox02.getItems().add("Maçãs");
-        comboBox02.getItems().add("Bananas");
-        comboBox02.getItems().addAll("Bacon", "Presunto", "Bolinho de carne");
+    @FXML
+    private void comboAction(ActionEvent event) {
+        comboBox02.getItems().clear();
+
+        System.out.println(comboBox01.getValue());
+        if(comboBox01.getValue() == "Magia"){
+            comboBox02.getItems().addAll("nomeMagia", "nivel", "tipo", "descricao"); //SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'some_table';
+        }else if(comboBox01.getValue() == "Item"){
+
+        }else if(comboBox01.getValue() == "Pericia"){
+
+        }else if(comboBox01.getValue() == "Talento"){
+
+        }else if(comboBox01.getValue() == "Antecedente"){
+
+        }else if(comboBox01.getValue() == "Deus"){
+
+        }
+
+    }
+    public void InsertChoiceBox(){
+        comboBox01.getItems().addAll("Magia", "Item", "Pericia", "Talento", "Antecedente", "Deus");
+        // SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema='public';
     }
 
     /*
