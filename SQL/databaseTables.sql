8f0d39d0f4172d6f22eda55cf4e2879b1e72cf77
IgorFNegrizoli <igor.negrizoli@gmail.com>
2021-09-13 11:53:41 -0300

    Fixed last commit mistake]

--- SQL/databaseTables.sql
+++ SQL/databaseTables.sql
@@ -310,7 +310,6 @@ create table Patrulheiro (
 
 create table ClasseMagia (
 	nomeClasse varchar(20),
-	nivel integer not null,
 	nomeMagia varchar(35),
 	primary key (nomeClasse, nomeMagia),
 	foreign key (nomeClasse) references Classe (nomeClasse),
