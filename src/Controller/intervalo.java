261a54a6804bb62c41a3782477ab4335abf039ba
AmandaIsrael <amanda_israel@hotmail.com>
2021-09-19 18:54:19 -0300

    busca01, busca02, intervalo e relatorio da campanha

--- src/Controller/intervalo.java
+++ src/Controller/intervalo.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import Model.DAO.MagiaDAO;
+import Model.Magia;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import sample.main;
 
 import java.net.URL;
@@ -20,12 +25,12 @@ public class intervalo implements Initializable, controlledScreen {
     @FXML
     TableColumn nivelGrupo;
     @FXML
-    Button pesquisar;
-    @FXML
     TextField barraPesquisa;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initTable();
+    }
 
     @Override
     public void setScreenParent(screensController screenPage) { myController = screenPage; }
@@ -34,4 +39,21 @@ public class intervalo implements Initializable, controlledScreen {
     private void goToCampanha(ActionEvent event){
         myController.setScreen(main.screen3ID);
     }
+
+    @FXML
+    private void Pesquisar(ActionEvent event){
+        tableView.getColumns().clear();
+        String pesquisa = barraPesquisa.getText();
+
+        //ObservableList<intervalo> nivel = FXCollections.observableArrayList(DAO.Search(pesquisa));
+        //tableView.setItems(nivel);
+    }
+
+    private void initTable(){
+        nivelGrupo.setCellValueFactory(new PropertyValueFactory<>("nivelGrupo"));
+
+        //ObservableList<intervalo> nivel = FXCollections.observableArrayList(DAO.List()));
+        //tableView.setItems(nivel);
+    }
+
 }
--- src/Controller/relatorioCampanhaController.java
+++ src/Controller/relatorioCampanhaController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import sample.main;
 
 import java.net.URL;
@@ -30,12 +31,12 @@ public class relatorioCampanhaController implements Initializable, controlledScr
     @FXML
     TableColumn numeroLideres;
     @FXML
-    Button pesquisar;
-    @FXML
     TextField barraPesquisa;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initTable();
+    }
 
     @Override
     public void setScreenParent(screensController screenPage) { myController = screenPage; }
@@ -43,5 +44,26 @@ public class relatorioCampanhaController implements Initializable, controlledScr
     @FXML
     private void goToCampanha(ActionEvent event){
         myController.setScreen(main.screen3ID);
+    }
+
+    @FXML
+    private void Pesquisar(ActionEvent event){
+        tableView.getColumns().clear();
+        String pesquisa = barraPesquisa.getText();
+
+        //ObservableList<relatorioCampanha> nomeCampanha = FXCollections.observableArrayList(DAO.Search(pesquisa));
+        //tableView.setItems(nomeCampanha);
+    }
+
+    private void initTable(){
+        nomeCampanha.setCellValueFactory(new PropertyValueFactory<>("campanha"));
+        descricao.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+        numeroCidades.setCellValueFactory(new PropertyValueFactory<>("numeroCidades"));
+        cidadeMaiorPopulacao.setCellValueFactory(new PropertyValueFactory<>("cidadeMaiorPopulacao"));
+        mediaPopulacaoCidades.setCellValueFactory(new PropertyValueFactory<>("mediaPopulacaoCidade"));
+        numeroLideres.setCellValueFactory(new PropertyValueFactory<>("numeroLideres"));
+
+        //ObservableList<relatorioCampanha> nomeCampanha = FXCollections.observableArrayList(DAO.List()));
+        //tableView.setItems(nomeCampanha);
     }
 }
