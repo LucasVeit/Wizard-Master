bbcfd9a143edd96f133b815e3eca8bccc60b538e
IgorFNegrizoli <igor.negrizoli@gmail.com>
2021-09-18 22:18:27 -0300

    Added Indexes and fixed query 05

--- SQL/consultas/05.sql
+++ SQL/consultas/05.sql
@@ -1,26 +1,28 @@
 --Listagem de líderes e quantas pessoas cada um governa
-select nomeLider as "Lider", sum(populacao) as "Populacao"
-from Lider join (
-	select codigoLider, populacao
-	from LiderCidade join (
-		select codigoCidade, populacao
-		from Cidade
-		) as foo
-		on LiderCidade.codigoCidade = foo.codigoCidade
-	) as foo2
-	on foo2.codigoLider = Lider.codigoLider
-	group by nomeLider
-	
-union
+select foo5.nomeLider as "Lider", sum(populacao) as "Populacao"
+from (
+	select nomeLider, populacao
+	from Lider join (
+		select codigoLider, populacao
+		from LiderCidade join (
+			select codigoCidade, populacao
+			from Cidade
+			) as foo
+			on LiderCidade.codigoCidade = foo.codigoCidade
+		) as foo2
+		on foo2.codigoLider = Lider.codigoLider
 
-select nomeLider as "Lider", sum(populacao) as "Populacao"
-from Lider join (
-	select codigoLider, populacao
-	from LiderFaccao join (
-		select codigoFaccao, populacao
-		from Faccao
-		) as foo
-		on LiderFaccao.codigoFaccao = foo.codigoFaccao
-	) as foo2
-	on foo2.codigoLider = Lider.codigoLider
-	group by nomeLider;
+	union
+
+	select nomeLider, populacao
+	from Lider join (
+		select codigoLider, populacao
+		from LiderFaccao join (
+			select codigoFaccao, populacao
+			from Faccao
+			) as foo3
+			on LiderFaccao.codigoFaccao = foo3.codigoFaccao
+		) as foo4
+		on foo4.codigoLider = Lider.codigoLider
+) as foo5
+group by "Lider"
